c2439728fd0667b40adfb644b9355be0b85d8076
vineetrshenoy <vineetrshenoy@users.noreply.github.com>
2019-10-20 15:53:47 -0400

    Adding code to input numerator/denominator

--- branch.m
+++ branch.m
@@ -1,8 +1,8 @@
-function output = branch(signal, H, F, r)
+function output = branch(signal, H_num, H_den, F_num, F_den, r)
 
-	X = filter(H, [1], signal);
+	X = filter(H_num, H_den, signal);
 	v = decim(X, r);
-	z = interpolat(v, 2);
-	Y = filter(F, [1], z);
+	z = interpolat(v, r);
+	Y = filter(F_num, F_den, z);
 	output = Y;
 
--- project1.m
+++ project1.m
@@ -5,14 +5,27 @@ A = double(A);
 figure(1)
 imshow(A, map)
 
-branch1 = branch(A, [2 1], [-3 2], 2);
-branch2 = branch(A, [3 2], [2 -1], 2);
+
+H0_num = [2 6 1 5 0 1];
+H0_den = [1];
+
+H1_num = [2 -6 1 -5 0 -1];
+H1_den = [1];
+
+F0_num = [2 6 1 5 0 1];
+F0_den = [12 0 16 0 7 0 1];
+
+F1_num = [-2 6 -1 5 0 1];
+F1_den = [12 0 16 0 7 0 1];
+
+branch1 = branch(A, H0_num, H0_den, F0_num, F0_den, 2);
+branch2 = branch(A, H1_num, H1_den, F1_num, F1_den, 2);
 
 
 x_hat = branch1 + branch2;
 
 figure(3)
-imshow(0.5*x_hat, map)
+imshow(0.25*x_hat, map)
 
 
 
